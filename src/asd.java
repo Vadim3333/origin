7b7c61448223642dc9e7e15ca60c518866cafc0c
375297158888 <newatlant@tut.by>
2021-05-04 19:10:42 +0300

    1st load

---
+++ src/asd.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class asd {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int a = console.nextInt();
+        int b = console.nextInt();
+        int c = console.nextInt();
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(c);
+        }
+
+    }
